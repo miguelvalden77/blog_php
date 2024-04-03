ebd1c9760d9ffa52e884bc48b160153d127982ea
miguelvalden <miguelvalden77@gmail.com>
2024-04-03 15:06:46 +0200

    deploy

--- includes/db.php
+++ includes/db.php
@@ -1,18 +1,9 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php'; // Carga Composer autoloader
-
-use Dotenv\Dotenv;
-
-// Carga el archivo .env
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-
-$server = $_ENV["monorail.proxy.rlwy.net"];
-$username = $_ENV["DB_HOST"];
-$password = $_ENV["DB_PASSWORD"];
-$database = $_ENV["DB_DATABASE"];
+$server = "monorail.proxy.rlwy.net";
+$username = "root";
+$password = "HjBPkzlITifclLrZzpwLrUFFNZgNbSNj";
+$database = "railway";
 
 $db = mysqli_connect($server, $username, $password, $database);
 
