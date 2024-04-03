9d3914220c0c100e49db5749c26d96478c714bec
miguelvalden <miguelvalden77@gmail.com>
2024-04-03 04:10:08 +0200

    env

--- includes/db.php
+++ includes/db.php
@@ -1,16 +1,23 @@
 <?php
 
-    $server = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "blog";
+require __DIR__ . '/vendor/autoload.php'; // Carga Composer autoloader
 
-    $db = mysqli_connect($server, $username, $password, $database);
+use Dotenv\Dotenv;
 
-    mysqli_query($db, "SET NAMES 'utf8'");
+// Carga el archivo .env
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
-    if(!isset($_SESSION)){
-        session_start();
-    }
 
-?>
+$server = $_ENV["monorail.proxy.rlwy.net"];
+$username = $_ENV["DB_HOST"];
+$password = $_ENV["DB_PASSWORD"];
+$database = $_ENV["DB_DATABASE"];
+
+$db = mysqli_connect($server, $username, $password, $database);
+
+mysqli_query($db, "SET NAMES 'utf8'");
+
+if (!isset($_SESSION)) {
+    session_start();
+}
